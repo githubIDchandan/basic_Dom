f96cd9ac4ae7f2dbe105b4c6fa1c6a97ec0451cd
chandanK <iamchandankumar02@gmail.com>
2023-05-31 14:49:39 +0530

    filter feature dala hai

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var form=document.getElementById('addForm')
 // form submit event;
 var ulList=document.getElementById('items');
+var filter=document.getElementById('filter');
 
 
 form.addEventListener('submit',addItem);
@@ -13,7 +14,9 @@ deleteB.addEventListener('click',removeItem);
 
 ulList.addEventListener('click',removeItem);
 
-
+filter.addEventListener('keyup',filterItems);
+// add category
+var category=document.getElementById('category');
 
 // add item
 function addItem(event){
@@ -22,13 +25,19 @@ event.preventDefault();
 // console.log(1)
 
     var Inpvalue=document.getElementById('item').value;
+    var categoryVal=" "
+    categoryVal+=category.value;
 
     var li=document.createElement('li');
     // console.log(li);
     li.className='list-group-item list-group-item-info';
     var nodeval=document.createTextNode(Inpvalue);
+    var nodeval2=document.createTextNode(categoryVal);
+    // nodeval+=nodeval2;
+    // console.log(nodeval)
     li.appendChild(nodeval);
-
+    li.appendChild(nodeval2)
+    console.log(li);
     var ulList=document.getElementById('items');
     ulList.appendChild(li);
     var editBtn=document.createElement('button');
@@ -44,8 +53,22 @@ event.preventDefault();
     li.appendChild(deleteBtn);
 
 
+ 
+
+
+
+
+
+
+
+
+
 
 }
+
+
+// add category
+
 
 
 // removeItem;
@@ -66,3 +89,38 @@ function removeItem(event){
 }
 
 }
+
+
+
+function filterItems(event){
+  var text=event.target.value.toLowerCase();
+  var text2=event.target.value.toLowerCase();
+
+//   console.log(text)
+//    ul ke under jitne bhi li item hai wo gaya items ke under==Htmlcollectio
+  var items=ulList.getElementsByTagName('li');
+//   console.log(items)
+// itemname2 is notworking here?
+// var itemname2=items.childNodes[1].textContent;
+// console.log(itemname2)
+
+Array.from(items).forEach(function(containtName){
+    // console.log(containt)
+    var itemname=containtName.firstChild.textContent;
+    var itemname2=containtName.childNodes[1].textContent;
+
+
+    // console.log(itemname2)
+    // console.log(text)
+    // console.log(scond)
+    if((itemname.toLowerCase().indexOf(text)!=-1)||(itemname2.toLowerCase().indexOf(text)!=-1)){
+        containtName.style.display='block';
+    }   
+    else{
+        containtName.style.display='none'
+    }
+
+});
+
+}
+
