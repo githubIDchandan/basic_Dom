4a21cf6da98028d7ea5e0b777faf92838581a709
chandanK <iamchandankumar02@gmail.com>
2023-05-31 20:21:23 +0530

    added detail on screen as well as in LocalStorage

--- form.js
+++ form.js
@@ -1,23 +1,37 @@
 
 
-// function submitForm(e){
-// var fname=document.getElementById('fname').value;
-// var UserEmail=document.getElementById('UserEmail').value;
-// var Userphn=document.getElementById('Userphn').value;
-// console.log(fname)
-// console.log(UserEmail)
-// console.log(Userphn)
-
-// localStorage.setItem('fname',fname);
-// localStorage.setItem('UserEmail',UserEmail);
-// localStorage.setItem('Userphn',Userphn);
+function submitForm(event){
+var fname=document.getElementById('fname').value;
+var UserEmail=document.getElementById('UserEmail').value;
+var Userphn=document.getElementById('Userphn').value;
+console.log(fname)
+console.log(UserEmail)
+console.log(Userphn)
+fname+=" ";
+UserEmail+=" ";
+Userphn+=" ";
+localStorage.setItem('fname',fname);
+localStorage.setItem('UserEmail',UserEmail);
+localStorage.setItem('Userphn',Userphn);
 // console.log(localStorage.getItem('fname'));
 
+var Ulitems=document.getElementById('items');
+var li=document.createElement('li');
+var nameNode=document.createTextNode(fname);
+var emailNode=document.createTextNode(UserEmail);
+var userNode=document.createTextNode(Userphn);
+
+li.appendChild(nameNode);
+li.appendChild(emailNode);
+li.appendChild(userNode);
+// console.log(li);
+Ulitems.appendChild(li)
 
 
 
 
-// }
+
+}
 
 
 /*
@@ -36,12 +50,12 @@ it is browser indepent ek mai ho gya to dusra mai nhi hoga.
 
 
 
-let myObj={
-    name:"domenic",
-    age:56
-}
+// let myObj={
+//     name:"domenic",
+//     age:56
+// }
 
-let myObj_seralized=JSON.stringify(myObj);
-localStorage.setItem("myObj",myObj_seralized);
-let myObj_dseralized=JSON.parse(localStorage.getItem("myObj"));
-console.log(myObj_dseralized)
+// let myObj_seralized=JSON.stringify(myObj);
+// localStorage.setItem("myObj",myObj_seralized);
+// let myObj_dseralized=JSON.parse(localStorage.getItem("myObj"));
+// console.log(myObj_dseralized)
